a6035b13eeb75ce80a985ffe2dbf2aec8635c2a8
Uriah Harston <uharston@gmail.com>
2020-06-11 15:56:17 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -15,6 +15,20 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      if @@cart.length == 0 
+        resp.write "Your cart is empty"
+      else 
+        @@cart.each { |i| resp.write "#{i}\n"}
+      end
+    elsif req.path.match(/add/)
+      item_to_add = req.params["item"]
+      if @@items.include?(item_to_add)
+        @@cart << item_to_add 
+        resp.write "added #{{item_to_add}}"
+      else 
+        resp.write "We don't have that item in stock"
+      end  
     else
       resp.write "Path Not Found"
     end
